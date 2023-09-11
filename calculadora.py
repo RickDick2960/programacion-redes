a39b64eecf56e5d4ef82a388e92fcdb981943590
RickDick2960 <ricardo.hernandez6808@gmail.com>
2023-09-11 11:44:19 -0600

    Calculadora

---
+++ calculadora.py
@@ -0,0 +1,30 @@
+'''
+Autor: Ricardo Hernandez
+Fecha: 11 Septiembre 
+Descripcion: Calculadora que suma
+'''
+
+a=input("Indoruce el primer valor: ")
+b=input("Introduce el segundo valor: ")
+print(type(a), type(b))
+print("Suma ", a + b )
+a=int(a)
+b=int(b)
+print(type(a), type(b))
+print("Suma ", a + b )
+
+print("Suma numeros reales", a + b )
+print(type(a), type(b))
+print("Suma relaes ", a + b )
+
+print("Resta")
+print(type(a), type(b))
+print("resta relaes ", a - b )
+
+print("Multiplicacion")
+print(type(a), type(b))
+print("resta relaes ", a * b )
+
+print("Divicion")
+print(type(a), type(b))
+print("resta relaes ", a / b )
