e53245c70a6f5c31e518fd409857d0089eae73c3
Ricardo Hernandez <142540550+RickDick2960@users.noreply.github.com>
2023-09-25 09:35:53 -0600

    Laboratorios-Modulo2

---
+++ Modulo2.0/2.1.1.11.py
@@ -0,0 +1,8 @@
+'''
+ Nombre: Jose Ricardo Hernandez Vazquez
+ Grupo: GIR0541
+ Fecha: 25 de septiembre del 2023
+ Descripcion: Ejercicios 2.1.1.11
+'''
+print("Estoy\n\"\"aprendiendo\"\"\n\"\"\"Python\"\"\"")
+
