54ecd5e4fb8a7d965bfe1d3ee44720c8f5e05315
Ricardo Hernandez <142540550+RickDick2960@users.noreply.github.com>
2023-09-25 11:35:34 -0600

    Problemas1,2

---
+++ unidad1_modulo1/Problema1.py
@@ -0,0 +1,10 @@
+'''
+ Nombre: Jose Ricardo Hernandez Vazquez
+ Grupo: GIR0541
+ Fecha: 25 de septiembre del 2023
+ Descripcion: Solucion de Problema 1
+'''
+resultado = (3 + 2 / 2.5) ** 2
+
+print("El resultado de la operacion es:", resultado)
+
---
+++ unidad1_modulo1/Problema2.py
@@ -0,0 +1,10 @@
+'''
+ Nombre: Jose Ricardo Hernandez Vazquez
+ Grupo: GIR0541
+ Fecha: 25 de septiembre del 2023
+ Descripcion: Solucion de Problema 2
+ '''
+ 
+n = int(input("Ingresa un numero entero: "))
+suma = n*(n+1)/2
+print("El resultado de la suma es:",int(suma))
