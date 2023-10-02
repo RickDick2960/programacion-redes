7fe8dc48a3e80654d129fc78bfdc9ca51514c564
Ricardo Hernandez <142540550+RickDick2960@users.noreply.github.com>
2023-10-02 17:56:18 -0600

    laboratorios_modulo3

---
+++ Modulo3/3.2.1.10.py
@@ -0,0 +1,25 @@
+﻿'''
+Nombre: Jose Ricardo Hernandez Vazquez
+ Grupo: GIR0541
+ Fecha: 01 de octubre del 2023
+ Ejercicio: 3.2.1.10
+'''
+
+
+# Indicar al usuario que ingrese una palabra
+# y asignarlo a la variable user_word.
+
+for letter in user_word:
+user_word = input("Ingrese una palabra: ")
+user_word = user_word.upper()
+
+letras_no_consumidas = ""
+
+for letter in user_word:
+    
+    if letter in "AEIOU":
+        continue
+    letras_no_consumidas += letter
+
+for letra in letras_no_consumidas:
+    print(letra)
---
+++ Modulo3/3.4.1.13.py
@@ -0,0 +1,30 @@
+﻿'''
+ Nombre: Jose Ricardo Hernandez Vazquez
+ Grupo: GIR0541
+ Fecha: 01 de octubre del 2023
+ Ejercicio:3.4.1.13
+'''
+
+# Paso 1: Crear una lista vacía llamada beatles
+beatles = []
+
+# Paso 2: Agregar los miembros de la banda usando append()
+beatles = ["John Lennon", "Paul McCartney", "George Harrison"]
+
+# Paso 3: Pedir al usuario que agregue más miembros usando un bucle for y append()
+miembros_add = ["Stu Sutcliffe", "Pete Best"]
+for miembro in miembros_add :
+    beatles.append(miembro)
+
+# Paso 4: Eliminar a Stu Sutcliffe y Pete Best usando del
+del beatles[3]  
+del beatles[3]  
+
+# Paso 5: Agregar a Ringo Starr al principio de la lista usando insert()
+beatles.insert(0, "Ringo Starr")
+
+# Imprimir la lista final
+print(beatles)
+
+# Probando la longitud de la lista
+print("Número de miembros de los Beatles:", len(beatles))
