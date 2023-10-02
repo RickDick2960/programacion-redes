d831cb45d263ef96ae345d71fc5eec16dd6f09c0
Ricardo Hernandez <142540550+RickDick2960@users.noreply.github.com>
2023-10-02 10:10:33 -0600

    laboratorios_modulo2

---
+++ Modulo2.0/2.1.1.18.py
@@ -0,0 +1,9 @@
+'''
+ Nombre: Jose Ricardo Hernandez Vazquez
+ Grupo: GIR0541
+ Fecha: 17 de septiembre del 2023
+ Descripcion: Ejercicios 2.1.1.18
+'''
+# Ejercicio 2.1.1.7
+print("Fundamentos","Programación","en",sep="***",end="...")
+print("Python")
---
+++ Modulo2.0/2.6.1.9.py
@@ -0,0 +1,28 @@
+
+
+
+'''
+ Nombre: Jose Ricardo Hernandez Vazquez
+ Grupo: GIR0541
+ Fecha: 25 de septiembre del 2023
+ Descripcion: Ejercicios 2.6.1.9
+'''
+
+
+
+a = float(input("Ingrese un valor para a: "))
+b = float(input("Ingrese un valor para b: "))
+
+# Muestra el resultado de la suma
+print("Suma:", a + b)
+
+# Muestra el resultado de la resta
+print("Resta:", a - b)
+
+# Muestra el resultado de la multiplicación
+print("Multiplicación:", a * b)
+
+# Muestra el resultado de la división
+print("División:", a / b)
+
+print("\n¡Eso es todo, amigos!")
