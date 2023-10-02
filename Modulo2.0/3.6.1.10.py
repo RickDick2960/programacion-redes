cc13c465dbe8e63d94bcd570a9126984aab3f94b
Ricardo Hernandez <142540550+RickDick2960@users.noreply.github.com>
2023-10-02 09:39:30 -0600

    Add files via upload

---
+++ Modulo2.0/3.6.1.10.py
@@ -0,0 +1,12 @@
+﻿'''
+ Nombre: Jose Ricardo Hernandez Vazquez
+ Grupo: GIR0541
+ Fecha: 01 de octubre del 2023
+ Descripcion: Ejercicios 2.6.1.10
+'''
+x = float(input("Ingresa el valor para x: "))
+
+# Escribe tu código aquí.
+y = 1/(x+1/(x+1/(x+1/x)))
+
+print("y =",  y)
